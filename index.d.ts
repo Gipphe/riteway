e33e4f58077de7079321d8b567e83252f6124edc
Victor Nascimento Bakke <gipphe@gmail.com>
2019-01-20 00:58:48 +0100

    wip: attempt at fixing types

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,15 @@
 declare module 'riteway' {
+  import tape from 'tape';
 
-  export function describe(label: string, TestFunction: TestFunction): void
+  export type describe = {
+    (label: string, TestFunction: TestFunction): void
+    only: tape.only
+    skip: tape.skip
+  }
+
+  export const describe: describe;
+
+  export default describe;
 
   export function Try<U extends any[], V>(fn: (...args: U) => V, ...args: U): any | Promise<any>
 
